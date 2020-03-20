0e53e1a7dad98463aa804f8b31ee5795ffa4e774
Ellen Watkins <watkins.ellen@gmail.com>
2020-03-20 18:59:08 +0000

    js test

---
+++ script.js
@@ -0,0 +1,27 @@
+
+                var test = new Date();
+                test = test.getDate();
+                var month = new Date();
+                month = month.getMonth() +1;
+                var daysQ;
+
+                function daysInQuarantine(){
+                if (month == 3) {
+                    daysQ = Math.abs(test - 15)
+                    console.log(daysQ);
+                    return daysQ;
+                } else if (month == 4) {
+                    daysQ = Math.abs(test - 15) + 15; 
+                    //return daysQ;
+                } else if (month == 5) {
+                    daysQ = Math.abs(test - 15) + 45; 
+                    //return daysQ;
+                } else if (month == 6) {
+                    daysQ = Math.abs(test - 15) + 76; 
+                    //return daysQ;
+                }}; 
+                function toPage(){
+                    let qIDspot = document.getElementById("qID");
+                    qIDspot.innerHTML += daysInQuarantine();
+                }
+                toPage();
